b0785bc2f12b18fde00e1a5aa34ca0a30367810f
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-22 09:25:51 -0700

    feat(services): add sql TaskService and Task model

---
+++ app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Models/Task.java
@@ -0,0 +1,12 @@
+package tooearly.neumont.edu.sqltaskmanager.Models;
+
+public class Task {
+    public Task() {
+    }
+
+    public int id;
+    public String name;
+    public String description;
+    public int color;
+    public int time_spent;
+}
