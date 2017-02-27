2c3517d385444c6c2c7d4c36d1c549f1d5548fbc
Daniel <dkuhrmeyer@gmail.com>
2017-02-26 22:47:42 -0700

    trying to get timer to work.... receiving a null pointer but Kyle said that brandon made changes and wanted to pull

--- app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/MainActivity.java
+++ app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/MainActivity.java
@@ -1,14 +1,20 @@
 package tooearly.neumont.edu.sqltaskmanager.Activities;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Timer;
+
 
 import tooearly.neumont.edu.sqltaskmanager.Models.Task;
 import tooearly.neumont.edu.sqltaskmanager.Models.TaskListAdapter;
@@ -19,10 +25,47 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Task> listItems=new ArrayList<>();
     TaskListAdapter adapter;
 
+    Button btnStart, btnStop;
+    TextView timerText;
+    Handler customerHandler = new Handler();
+    long startTime=0L, timeInMilliseconds=0L, timeSwapBuff=0L, updateTime=0L;
+    Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
+            updateTime = timeSwapBuff+timeInMilliseconds;
+            int secs = (int)updateTime/1000;
+            int mins=secs/60;
+            secs%=60;
+            int milliseconds=(int)(updateTime%1000);
+            timerText.setText(""+mins+":"+String.format("%2d", secs) + ":" + String.format("%3d", milliseconds));
+            customerHandler.postDelayed(this, 0);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button)findViewById(R.id.startButton);
+        btnStop = (Button)findViewById(R.id.stopButton);
+        timerText = (TextView)findViewById(R.id.textViewTime);
+
+        btnStart.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                startTime = SystemClock.uptimeMillis();
+                customerHandler.postDelayed(updateTimerThread, 0);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                timeSwapBuff+=timeInMilliseconds;
+                customerHandler.removeCallbacks(updateTimerThread);
+            }
+        });
 
         taskService = new TaskService(this);
 
