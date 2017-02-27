a7752892e05b8259f8bd7c62a847233d36f2c64c
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-26 22:10:38 -0700

    feat(edit-task): view and edit task details

--- app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/EditTaskActivity.java
+++ app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/EditTaskActivity.java
@@ -3,6 +3,9 @@ package tooearly.neumont.edu.sqltaskmanager.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import tooearly.neumont.edu.sqltaskmanager.Models.Task;
 import tooearly.neumont.edu.sqltaskmanager.R;
@@ -19,17 +22,37 @@ public class EditTaskActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         int taskId = intent.getIntExtra(MainActivity.EXTRA_TASK_ID, 0);
-        if (taskId == 0) task = new Task();
-        else task = taskService.findById(taskId);
 
-        init();
+        init(taskId);
     }
 
-    private void init() {
+    private void init(int taskId) {
+        if (taskId == 0) task = new Task();
+        else {
+            TextView title = (TextView)findViewById(R.id.lbl_title);
+            title.setText(R.string.title_edit_task);
+            task = taskService.findById(taskId);
+        }
 
+        EditText taskName = (EditText)findViewById(R.id.taskName);
+        taskName.setText(task.name);
+
+        EditText taskDescription = (EditText)findViewById(R.id.taskDescription);
+        taskDescription.setText(task.description);
     }
 
     private TaskService taskService;
 
     public Task task;
+
+    public void submitClicked(View view) {
+        EditText taskName = (EditText)findViewById(R.id.taskName);
+        task.name = taskName.getText().toString();
+
+        EditText taskDescription = (EditText)findViewById(R.id.taskDescription);
+        task.description = taskDescription.getText().toString();
+
+        taskService.update(task);
+        finish();
+    }
 }
--- app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/TaskDetailsActivity.java
+++ app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/TaskDetailsActivity.java
@@ -3,6 +3,9 @@ package tooearly.neumont.edu.sqltaskmanager.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import tooearly.neumont.edu.sqltaskmanager.Models.Task;
 import tooearly.neumont.edu.sqltaskmanager.R;
@@ -19,17 +22,42 @@ public class TaskDetailsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         int taskId = intent.getIntExtra(MainActivity.EXTRA_TASK_ID, 0);
+
+        init(taskId);
+    }
+
+    private void init(int taskId) {
         if (taskId == 0) throw new IllegalArgumentException("No task with ID " + taskId);
         else task = taskService.findById(taskId);
 
-        init();
-    }
+        updateCompletedCheckbox();
 
-    private void init() {
+        TextView taskName = (TextView) findViewById(R.id.taskName);
+        taskName.setText(task.name);
+
+        TextView taskDescription = (TextView)findViewById(R.id.taskDescription);
+        taskDescription.setText(task.description);
+    }
+    private void updateCompletedCheckbox() {
 
     }
 
     private TaskService taskService;
 
     private Task task;
+
+    public void onToggleClicked(View view) {
+        task.completed = !task.completed;
+        taskService.update(task);
+        updateCompletedCheckbox();
+    }
+    public void editTaskClicked(View view) {
+        Intent intent = new Intent(this, EditTaskActivity.class);
+        intent.putExtra(MainActivity.EXTRA_TASK_ID, task.id);
+        startActivity(intent);
+    }
+    public void onDeleteClicked(View view) {
+        taskService.delete(task.id);
+        finish();
+    }
 }
