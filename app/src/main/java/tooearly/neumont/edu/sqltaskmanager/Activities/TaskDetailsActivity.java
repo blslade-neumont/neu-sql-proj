ba3144beab8819297a7a6f6a6e16b600b3eac9c8
aboveyou00 <brandonyoyoslade@gmail.com>
2017-02-26 21:16:23 -0700

    feat(activities): add activities for task details and creation layouts

---
+++ app/src/main/java/tooearly/neumont/edu/sqltaskmanager/Activities/TaskDetailsActivity.java
@@ -0,0 +1,13 @@
+package tooearly.neumont.edu.sqltaskmanager.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
